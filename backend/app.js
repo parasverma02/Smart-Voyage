da3152c9ca22fb659334e026bfe7af83275eae8a
Shashikanth K S <sks@uwaterloo.ca>
2020-01-31 15:47:30 -0500

    Adding login

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,10 @@ app.get('/', (req,res) => {
     res.send("This will be the response");
 });
 
+app.get('/api/login', (req,res) => {
+    res.send("Authenticating user");
+})
+
 var port = 3000;
 app.listen(port, () => 
 console.log("Listening to port", port)
