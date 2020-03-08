5ff26c0467ee7883ebdc41cf7471dc973727e123
Shashikanth K S <sks@uwaterloo.ca>
2020-03-07 23:46:23 -0500

    optimized search function

--- backend/search.py
+++ backend/search.py
@@ -5,8 +5,7 @@ from pymongo import MongoClient
 client = MongoClient(port=27017)
 db=client.flightInfo
 city_dict = dict()
-
-
+flights = []
 
 def main():
     print("Executing python script")
@@ -14,7 +13,8 @@ def main():
     print("data:",data["cities"])
     i=0
     date = data["startdate"]
-    print(date[0]+1)
+    source = db.airportcodes.find_one({'city':data["sourcecity"]})
+    print(source)
     for city in data["cities"]:
         print(city["city"])
         airport = db.airportcodes.find_one({'city': city["city"]})
@@ -22,31 +22,21 @@ def main():
         city_dict[i] = [airport["code"], city["days"]]
         i = i+1
     print("dictionary is ", city_dict)
-    test(date)
-    # search(city_dict)
+    search(source["code"], date, city_dict)
     # sys.stdout.flush()
 
-def test(date):
-    x = list(db.documents.find({"departureAirportFsCode": "YYZ", "arrivalAirportFsCode": "YVR", "departureTime": date[0]}).sort("flightcost",1))
-    # x = list(db.documents.find({departureAirportFsCode:"YYZ"}))
-    print(x)
-
-# def search(cities):
-    # for key in cities:
-    #     print(len(cities) , cities[key][0])
-    #     doc = list(db.documents.aggregate(
-    #     [
-    #         { $match: {arrivalAirportFsCode: cities[key][0]} }
-    #         {
-    #         $group:
-    #         {
-    #             _id: {},
-    #             flightcost: { $min: "$flightcost" }
-    #         } 
-    #         }
-    #     ]
-    #     ))
-    #     print("doc is ", doc)
+def search(source, date, cities):
+    start = source
+    print(start)
+    for key in cities:
+        x = list(db.documents.find({"departureAirportFsCode": start, "arrivalAirportFsCode": cities[key][0]}).sort("flightcost",1).limit(1))
+        flights.append(x[0])
+        start = cities[key][0]
+    print(start, source)
+    y = list(db.documents.find({"departureAirportFsCode": start, "arrivalAirportFsCode": source}).sort("flightcost",1).limit(1))
+    print(y)
+    # flights.append(x[0])
+    print("Flight info:", flights)
 
 if __name__ == '__main__':
     main() 
