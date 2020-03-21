38aa52ede919ea07e60fcfa93ed942e46e6ebff4
Shashikanth K S <sks@uwaterloo.ca>
2020-03-21 18:04:09 -0400

    Modularity in python module

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const app = express();
-
-const MongoClient = require('mongodb').MongoClient;
-const mongourl = require('./config.js');
-
-var cities;
 var util = require("util");
 var childprocess = require("child_process");
+var authcontroller = require('./controllers/authentication')
+var setupcontroller = require('./controllers/setup')
+var testcontroller = require('./controllers/test')
+
 var spawn = require("child_process").spawn;
-
-
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); 
@@ -23,131 +19,9 @@ app.use(function(req, res, next) {
     next();
 });
 
-
-app.get('/', (req,res) => {
-    res.send("This will be the response");
-});
-
-app.get('/api/setup', function(req,res){
-    MongoClient.connect("mongodb://localhost:27017/", function(err,database){
-        if (err) throw err;
-        console.log("Connected to db");
-        var cities = [
-            {
-                city:"toronto",
-                code: "YYZ"
-            },
-            // {
-            //     city:"calgary",
-            //     code: "YYC"
-            // },
-            {
-                city:"montreal",
-                code: "YUL"
-            },
-            // {
-            //     city:"ottawa",
-            //     code: "YOW"
-            // },
-            {
-                city:"vancouver",
-                code: "YVR"
-            },
-            {
-                city:"winnipeg",
-                code: "YWG"
-            },
-            // {
-            //     city:"quebec",
-            //     code: "YQB"
-            // },
-            // {
-            //     city:"saskathoon",
-            //     code: "YXE"
-            // },
-            // {
-            //     city:"halifax",
-            //     code: "YHZ"
-            // },
-            {
-                city:"edmonton",
-                code: "YEG"
-            }
-        ];
-        const db = database.db('flightInfo')
-
-        const collection = db.collection('airportcodes')
-        collection.insert(cities)
-        res.json({ message: "Success"});
-    })
-})
-app.post('/api/signup', (req,res) => {
-    MongoClient.connect("mongodb://localhost:27017/", function(err,database){
-        if (err) throw err;
-        console.log("Connected to db");
-        console.log("body is ", req.body);
-        var user = {
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            phonenumber: req.body.phonenumber
-        };
-        const db = database.db('customerdb')
-
-        const collection = db.collection('userdetails')
-
-
-        collection.findOne({username:req.body.username}, function(err, result){
-            console.log("result is ", result);
-            if (err){
-                throw err;
-            } 
-            if(result){
-                console.log("user exists!");
-                res.json({ message: "Username is taken. Please try another one"});
-            }
-            else{
-                collection.insertOne(user);
-                console.log("User inserted!");
-                res.json({ message: "Success"});
-            }
-            
-        })
-
-    });
-})
-
-
-app.post('/api/signin', (req,res) => {
-
-    MongoClient.connect("mongodb://localhost:27017/", function(err,database){
-        if (err) throw err;
-        console.log("Connected to db");
-        console.log("body is ", req.body);
-        const db = database.db('customerdb')
-        const collection = db.collection('userdetails')
-        collection.findOne({username:req.body.username, password:req.body.password}, function(err, result){
-            console.log("result is ", result);
-            if(result){
-                console.log("Login Successful!");
-                res.json({ 
-                    success: true,
-                    message: "Login Successful!"
-                });
-            }
-            else{
-                console.log("Invalid username or password!");
-                res.json({ 
-                    success: false,
-                    message: "Invalid username or password"
-                });
-            }
-            
-        })
-    });
-})
+authcontroller(app);
+setupcontroller(app);
+testcontroller(app);
 
 app.get('/api/cityinfo', function(req,res){
     console.log("sending city info")
@@ -164,10 +38,14 @@ app.post('/api/search', function(req,res){
     process.stdin.end();
 
     process.stdout.on('data',function(data){
-        util.log(data.toString());
-        res.json(data.toString());
+        var stringres = data.toString();
+        var jsonres = JSON.parse(JSON.stringify(stringres))
+        util.log(jsonres);
+        res.send(jsonres);
     });
-
+    process.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
     childprocess.exec('python search.py', function (err){
         if (err) {
             console.log("child processes failed with error code: " + err.code);
--- backend/search.py
+++ backend/search.py
@@ -11,7 +11,6 @@ client = MongoClient(port=27017)
 db=client.flightInfo
 city_dict = dict()
 route = []
-mincost = 10000000
 totalcost = {}
 data = {}
 
@@ -29,12 +28,14 @@ def main():
         city_dict[i] = [airport["code"], city["days"]]
         i = i+1
     i=0
-    bruteforce(source["code"], startdate, city_dict)
+    final_route , mincost = bruteforce(source["code"], startdate, city_dict)
+    formatjson(final_route, mincost)
+
 
 def bruteforce(source, date, cities):
-    global mincost
-    global route
-    global totalcost
+    mincost = 100000000
+    flight_route = []
+    final_route = []
     l = list(permutations(range(0, len(cities))))
     for perm in l:
         val=0
@@ -44,18 +45,24 @@ def bruteforce(source, date, cities):
             val+=1
         val = 0
         # print(source, date)
-        # print(temp_dict)
         sorted_temp_dict = collections.OrderedDict(sorted(temp_dict.items()))
-        search(source, date, sorted_temp_dict)
-        # print("proceeding...")
-    totalcost['totalcost'] = mincost
-    # route.append(totalcost)
-    # print(route)
-    formatjson(route, totalcost)
+        flight_route = search(source, date, sorted_temp_dict)
+        # print(flight_route)
+        if(flight_route==None):
+            pass
+        else:
+            tempcost = 0
+            for flight in flight_route:
+                # print("f = ",flight['flightcost'])
+                tempcost += flight['flightcost']
+            # print("tempcost:", tempcost)
+            if(tempcost<mincost):
+                final_route = list(flight_route)
+                mincost = tempcost
+                # print("mincost updated:", mincost)
+    return final_route, mincost
 
 def search(source, date, cities):
-    global route
-    global mincost
     start = source
     d1 = getDateTimeFromISO8601String(date)
     d2 = d1 + timedelta(days=1)
@@ -63,8 +70,8 @@ def search(source, date, cities):
     for key in cities:
         x = list(db.documents.find({"departureAirportFsCode": start, "arrivalAirportFsCode": cities[key][0], "departureTime" : {"$gte":d1}, "arrivalTime" : {"$lt":d2}}).sort("flightcost",1).limit(1))
         if(x==[]):
-            # print(start, cities[key][0])
-            return
+            # print(start, cities[key][0], d1, d2)
+            return None
         flights.append(x[0])
         start = cities[key][0]
         num_days = cities[key][1]
@@ -74,37 +81,35 @@ def search(source, date, cities):
     if(y!=""):
         flights.append(y[0])
     # print(flights)
-    tempcost = 0
-    for flight in flights:
-        tempcost += flight['flightcost']
-    # print("tempcost:", tempcost)
-    if(tempcost<mincost):
-        route = list(flights)
-        mincost = tempcost
-        # print("mincost updated")
+    return flights
 
 
-def formatjson(finalroute, cost):
+def formatjson(final_route, cost):
     global data
     route = []
     flights = []
     result = {}
     flag = True
-    for obj in finalroute:
+    for obj in final_route:
         info = {}
         depcity = db.airportcodes.find_one({'code': obj["departureAirportFsCode"]})
         if(flag):
             source = depcity['city']
             flag = False
+        td = obj["arrivalTime"] - obj["departureTime"]
+        days = td.days
+        hours, remainder = divmod(td.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        travel_duration = str(hours) + 'h ' + str(minutes) + 'm'
         arrivalcity = db.airportcodes.find_one({'code': obj["arrivalAirportFsCode"]})
         route.append(depcity['city'])
         info["source"] = depcity['city']
         info["destination"] = arrivalcity['city']
         info["departureAirportFsCode"] = obj["departureAirportFsCode"]
         info["arrivalAirportFsCode"] = obj["arrivalAirportFsCode"]
-        info["totalFlightTime"] = obj["arrivalTime"] - obj["departureTime"]
-        info["departureTime"] = obj["departureTime"]
-        info["arrivalTime"] = obj["arrivalTime"]
+        info["totalFlightTime"] = travel_duration
+        info["departureTime"] = obj["departureTime"].isoformat()
+        info["arrivalTime"] = obj["arrivalTime"].isoformat()
         info["flightcost"] = obj["flightcost"]
         info["carrierFsCode"] = obj["carrierFsCode"]
         info["flightNumber"] = obj["flightNumber"]
@@ -113,10 +118,10 @@ def formatjson(finalroute, cost):
     route.append(source)
     result["route"] = list(route)
     result["flights"] = list(flights)
-    result["totalcost"] = cost["totalcost"]
+    result["totalcost"] = cost
     result["adults"] = data["adults"]
     result["children"] = data["children"]
-    print(result)
+    print(json.dumps(result))
 
 
 def getDateTimeFromISO8601String(s):
