108d04d5965024e4ba4a72961c515ed38f731f1d
Aditi Patade <apatade@uwaterloo.ca>
2020-04-07 03:37:32 -0400

    Update addtravellerdetails.component.spec.ts

--- frontend/src/app/home/profilepage/addtravellerdetails/addtravellerdetails.component.spec.ts
+++ frontend/src/app/home/profilepage/addtravellerdetails/addtravellerdetails.component.spec.ts
@@ -3,9 +3,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddtravellerdetailsComponent } from './addtravellerdetails.component';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
+import { ProfilepageService } from '../profilepage.service';
+import { of } from 'rxjs';
+
 describe('AddtravellerdetailsComponent', () => {
   let component: AddtravellerdetailsComponent;
   let fixture: ComponentFixture<AddtravellerdetailsComponent>;
+  let service: ProfilepageService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,6 +23,7 @@ describe('AddtravellerdetailsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AddtravellerdetailsComponent);
     component = fixture.componentInstance;
+    service = TestBed.get(ProfilepageService);
     fixture.detectChanges();
   });
 
@@ -28,13 +33,29 @@ describe('AddtravellerdetailsComponent', () => {
   it('should execute onSave()',() =>{
     // spyOn(component, 'onSignup');
     // const mockUser={firstname:"Aditi",lastname:"P",username:"aditi3049",password:"123456",emailid:"aditi3049@email.com",phonenumber:"9967650280"}
-    component.onSave();
+    // component.onSave();
     fixture.detectChanges();
 
     //expect(component.onSignup).toHaveBeenCalled();
 
 
   });
+  it('#onSave() called once',() => {
+    const response = {
+      message:"Success"
+      // firstname:"",
+      // lastname:"",
+      // username:"",
+      // emailid:"",
+      // password:"",phonenumber:"",dob:"",phone:"",gender:""
+    };
+    spyOn(component,'onSave').and.callThrough();
+    spyOn(service,'addnewtravller').and.returnValue(of(response));
+    component.onSave();
+    fixture.detectChanges();
+    expect(component.onSave).toHaveBeenCalledTimes(1);
+
+  })
 
 
 });
