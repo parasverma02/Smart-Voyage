0b9ba398a47c834069d4b470de8b89b331316f23
Aditi Patade <apatade@uwaterloo.ca>
2020-04-07 00:39:59 -0400

    Update travellerdetails.component.spec.ts

--- frontend/src/app/home/profilepage/travellerdetails/travellerdetails.component.spec.ts
+++ frontend/src/app/home/profilepage/travellerdetails/travellerdetails.component.spec.ts
@@ -28,6 +28,33 @@ describe('TravellerdetailsComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should execute functions',() =>{
+    // spyOn(component, 'onSignup');
+    // const mockUser={firstname:"Aditi",lastname:"P",username:"aditi3049",password:"123456",emailid:"aditi3049@email.com",phonenumber:"9967650280"}
+    //     "id":1,
+//     "firstname":"FirstName",
+//     "lastname":"LastName",
+//     "dob":"1994-11-30",
+//     "gender":"Female",
+//     "emailid":"aditi@email.com",
+//     "phoneno":"9967650280"
+    // component.travellerdetails.firstname="firstname"
+    // component.travellerdetails.lastname="lastname"
+    // component.travellerdetails.dob="dob"
+    // component.travellerdetails.gender="gender"
+    // component.travellerdetails.emailid="emailid"
+
+    component.onDataChanged([{"firstname":"User"}]);
+    // component.travellerdetails.length=5;
+    component.deleteRow("jane");
+
+
+    fixture.detectChanges();
+
+    //expect(component.onSignup).toHaveBeenCalled();
+
+
+  });
 
 
 
